14f75055fa24f78079edb2e7489481fc8451d9cb
Layt27 <laytonchetty@gmail.com>
2024-05-04 15:34:18 +0400

    update: profile page layout

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,10 +1,55 @@
 // Imports
-import React from 'react';
+import React, {useState} from 'react';
 
 const Profile = () => {
+  // State variables
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  }
+
+  const handleConfirm = () => {
+    setIsEditing(false);
+  }
+
+  const handleCancel = () => {
+    setIsEditing(false);
+  }
+
   return (
     <div className='profileDiv'>
-        <h1>This is the Profile page!</h1>
+      <h1>This is the Profile page!</h1>
+
+      <div className='profile-details-container'>
+        <h3>Account Details</h3>
+        <div className='form-group'>
+          <label>Name</label>
+          <input className='profile-input' type='text' value='Jim Tool'></input>
+        </div>
+
+        <div className='form-group'>
+          <label>Email</label>
+          <input className='profile-input' type='text' value='abc@123.com'></input>
+        </div>
+
+        <div className='form-group'>
+          <label>Phone number</label>
+          <input className='profile-input' type='text' value='+971569876543'></input>
+        </div>
+
+        {
+          !isEditing ? (
+            <button id='edit-button' className='profile-buttons' onClick={handleEdit} type='button'>Edit</button>
+          ) : (
+            <>
+            <button id='cancel-button' className='profile-buttons' onClick={handleCancel} type='button'>Cancel</button>
+            <button id='confirm-button' className='profile-buttons' onClick={handleConfirm} type='button'>Confirm</button>
+            </>
+          )
+        }
+
+      </div>
     </div>
   );
 };
