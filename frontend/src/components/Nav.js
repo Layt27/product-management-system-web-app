5e368fa36a1bee28eda27e76389f4a32e2464402
Layt27 <laytonchetty@gmail.com>
2024-05-09 00:06:39 +0400

    refactor: Remove the logo from the nav bar when user is not logged in

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -13,16 +13,16 @@ const Nav = () => {
     };
 
     return(
-        <div>
-            <a href='/'>
-                <img src='https://seekvectorlogo.com/wp-content/uploads/2018/01/enterprise-products-vector-logo.png'
-                    alt='logo'
-                    className='logo'
-                ></img>
-            </a>
-            
+        <div> 
             {
                 auth ?
+                    <>
+                    <a href='/'>
+                        <img src='https://seekvectorlogo.com/wp-content/uploads/2018/01/enterprise-products-vector-logo.png'
+                            alt='logo'
+                            className='logo'
+                        ></img>
+                    </a>
                     <ul className='nav-ul'>
                         <li><Link to="/">Products</Link></li>
                         <li><Link to="/add-product">Add Product</Link></li>
@@ -30,11 +30,13 @@ const Nav = () => {
                         <li style={{'float': 'right'}}><Link onClick={logout} to="/login">Log out ({JSON.parse(auth).name})</Link></li>
                         <li style={{'float': 'right'}}><Link to={"/profile/" + JSON.parse(auth)._id}>Profile</Link></li>
                     </ul>
+                    </>
                 :
                     <ul className='nav-ul nav-right'>
                         <li><Link to="/login">Log in</Link></li>
                         <li><Link to="/signup">Sign up</Link></li>
                     </ul>
+                    
             }
         </div>
     );
