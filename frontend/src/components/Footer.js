a2cf7dd7d63c74868e7d0f73a1f2665e9199cd06
Layt27 <laytonchetty@gmail.com>
2024-05-12 16:24:25 +0400

    style: Change heading tag of footer content to h4

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className='footer'>
-            <h3>&copy;2024, Enterprise Products, All rights reserved</h3>
+            <h4>&copy;2024, Enterprise Products, All rights reserved</h4>
         </div>
     );
 };
