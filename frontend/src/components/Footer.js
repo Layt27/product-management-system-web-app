651f2ad9116d1b77763419cc2a2df0132cb62e2d
Layt27 <laytonchetty@gmail.com>
2024-05-12 15:40:58 +0400

    refactor: Update footer content / Add CSS to footer content

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className='footer'>
-            <h3>E-comm Dashboard</h3>
+            <h3>&copy;2024, Enterprise Products, All rights reserved</h3>
         </div>
     );
 };
