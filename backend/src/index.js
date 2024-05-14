1ba69b6f74f37659c2fc2975e6ac63e317a127a3
Layt27 <laytonchetty@gmail.com>
2024-05-14 19:08:25 +0400

    refactor: Update cors middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors({
     origin: 'https://product-management-system-wa.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true  // Enable credentials (cookies, authorization headers, etc.)
+    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 }));
 
 const Jwt = require('jsonwebtoken');
