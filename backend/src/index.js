148e083ef8f5eea44e12f509a02297934446a40f
Layt27 <laytonchetty@gmail.com>
2024-05-15 12:31:08 +0400

    refactor: Update cors middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
     origin: 'https://product-management-system-wa.vercel.app',
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     optionsSuccessStatus: 200
 }));
